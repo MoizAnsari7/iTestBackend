7e55b8c46d849b15bc5992b956ef81676453ccc5
MoizAnsari7 <moizansari176@gmail.com>
2024-05-21 14:36:52 +0530

    Tests controller added with apis

---
+++ controllers/test.controller.js
@@ -0,0 +1,21 @@
+const CreateTest = (req, res )=>{
+
+}
+
+const TestList = (req, res )=>{
+    
+}
+
+const TestDetails = (req, res )=>{
+    
+}
+
+const UpdateTestDetails = (req, res )=>{
+    
+}
+
+const DeleteTest = (req, res )=>{
+    
+}
+
+module.exports = { CreateTest, TestList, TestDetails, UpdateTestDetails, DeleteTest }
