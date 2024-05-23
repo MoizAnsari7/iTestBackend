b3a4c86afea6abd1f9600c148ccc1902cb308ee9
MoizAnsari7 <moizansari176@gmail.com>
2024-05-23 18:21:26 +0530

    user answer api crud completed

--- controllers/answer.controller.js
+++ controllers/answer.controller.js
@@ -1,24 +1,59 @@
 const AnswerModel = require("../models/answer.model");
 
-const SubmitAnswer = ( req, res )=>{
-
+const SubmitAnswer = async ( req, res )=>{
+    try{
+        const { userId, questionId, selectedOptionId, responseTime } = req.body;
+    
+        let userAnswer = new AnswerModel({ userId, questionId, selectedOptionId, responseTime })
+    
+        //save assessment
+        let userAnswerDetails = await userAnswer.save();
+        return res.status(201).json({ msg : "user response saved successfully", data : userAnswerDetails })
+    
+    }catch(e){
+        console.log("catch SubmitAnswer : ",e);
+        return res.status(500).json({ message : "Internal server error", error : e })
+    }
 }
 
-const GetUserAnswerOfQuestionByQuestionId = ( req, res )=>{
-    
+const GetUserAnswerOfQuestionByQuestionId = async ( req, res )=>{
+    try{
+        let { userId, questionId } = request.params;
+        let userAnswer = await AnswerModel.find({ userId, questionId  }).pupulate()
+        if(!userAnswer)
+            {
+                return res.status(404).json({ message : "Answer Not Found" })
+            }
+            return res.status(404).json({ message : "Answer fetched successfully", data : userAnswer })
+    }catch(e){
+        console.log("catch CreateAssessment : ",e);
+        return res.status(500).json({ message : "Internal server error", error : e })
+    }
 }
 
 const  GetUserAnswerOfAllTestQuestion = ( req, res )=>{
-    
+    try{
+    }catch(e){
+        console.log("catch CreateAssessment : ",e);
+        return res.status(500).json({ message : "Internal server error", error : e })
+    }
 }
 
 const UpdateResponse = ( req, res )=>{
-    
+    try{
+    }catch(e){
+        console.log("catch CreateAssessment : ",e);
+        return res.status(500).json({ message : "Internal server error", error : e })
+    }
 }
 
 
 const DeleteResponse = ( req, res )=>{
-    
+    try{
+    }catch(e){
+        console.log("catch CreateAssessment : ",e);
+        return res.status(500).json({ message : "Internal server error", error : e })
+    }
 }
 
 
--- controllers/test.controller.js
+++ controllers/test.controller.js
@@ -1,21 +1,41 @@
 const CreateTest = (req, res )=>{
-
+    try{
+    }catch(e){
+        console.log("catch CreateAssessment : ",e);
+        return res.status(500).json({ message : "Internal server error", error : e })
+    }
 }
 
 const TestList = (req, res )=>{
-    
+    try{
+    }catch(e){
+        console.log("catch CreateAssessment : ",e);
+        return res.status(500).json({ message : "Internal server error", error : e })
+    }    
 }
 
 const TestDetails = (req, res )=>{
-    
+    try{
+    }catch(e){
+        console.log("catch CreateAssessment : ",e);
+        return res.status(500).json({ message : "Internal server error", error : e })
+    }
 }
 
 const UpdateTestDetails = (req, res )=>{
-    
+    try{
+    }catch(e){
+        console.log("catch CreateAssessment : ",e);
+        return res.status(500).json({ message : "Internal server error", error : e })
+    }
 }
 
 const DeleteTest = (req, res )=>{
-    
+    try{
+    }catch(e){
+        console.log("catch CreateAssessment : ",e);
+        return res.status(500).json({ message : "Internal server error", error : e })
+    }
 }
 
 module.exports = { CreateTest, TestList, TestDetails, UpdateTestDetails, DeleteTest }
