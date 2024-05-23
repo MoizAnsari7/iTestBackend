52ba1a08b5dbbab341e24618eeb7f057ae621cdc
MoizAnsari7 <moizansari176@gmail.com>
2024-05-23 16:50:37 +0530

    Controller for answer options

---
+++ controllers/ansOption.controller.js
@@ -0,0 +1,29 @@
+const AnsOptionModel = require("../models/answerOption.model");
+
+const SaveAnswer = ( req , res )=>{
+
+}
+
+const FindAllOptionByQuestionId = ( req , res )=>{
+    
+}
+
+const FindExplainationOfRightAnswer = ( req , res )=>{
+    
+}
+
+const updateOptionDetails = ( req , res )=>{
+    
+}
+
+const deleteOption  = ( req , res )=>{
+    
+}
+
+module.exports = {
+    SaveAnswer,
+    FindAllOptionByQuestionId,
+    FindExplainationOfRightAnswer,
+    updateOptionDetails,
+    deleteOption
+}
