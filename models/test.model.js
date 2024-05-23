e4dd8722a56872acb18d72189a4b6613b3b44d09
MoizAnsari7 <moizansari176@gmail.com>
2024-05-23 16:10:04 +0530

    Question Model created for question apis

--- models/test.model.js
+++ models/test.model.js
@@ -3,8 +3,8 @@ const { model, Schema } = require("mongoose")
 const TestSchema = new Schema({
     testName : String,
     description : String,
-    createdBy : { Type : Schema.Types.ObjectId, ref : "users" },
-    TimeLimit : Number, //time in minutes
+    createdBy : { type : Schema.Types.ObjectId, ref : "users" },
+    timeLimit : Number, //time in minutes
 },{
     timestamps : true,
     toJSON : { virtuals: true },
