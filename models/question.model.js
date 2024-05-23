2ecbe4ea1ddeb8781b789d4f718ee7e8b5c48728
MoizAnsari7 <moizansari176@gmail.com>
2024-05-23 16:35:15 +0530

    Question type changed from string to enum datatype
    Question type changed from string to enum datatype

--- models/question.model.js
+++ models/question.model.js
@@ -2,7 +2,7 @@ const { Schema, model } = require("mongoose")
 
 const QuestionSchema = new Schema({
     question : String,
-    questionType : String,
+    questionType : { type : String, enum : [ "MCQ4", "MCQ2","CODING","EASSAY", "VOICE_ANSWER" ], default : "MCQ4",required : true },
     testId : { type : Schema.Types.ObjectId, ref : "tests" },
     difficultyLevel : { type : String, enum : ["EASY", "MEDIUM", "HARD"], default : "EASY", required : true},//enum 
     createdBy : { type : Schema.Types.ObjectId, ref : "users" }
