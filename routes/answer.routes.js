7de7080b4515981acfebd8a2598befcc047338f3
MoizAnsari7 <moizansari176@gmail.com>
2024-05-23 17:36:40 +0530

    Answer routes added

---
+++ routes/answer.routes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const { SubmitAnswer, GetUserAnswerOfQuestionByQuestionId, GetUserAnswerOfAllTestQuestion, UpdateResponse, DeleteResponse } = require("../controllers/answer.controller");
+
+//save response of a question as answer
+router.post("/save", SubmitAnswer);
+
+//
+router.get("/answer/:questionId", GetUserAnswerOfQuestionByQuestionId);
+
+//find whole test as test copy filled by the user.
+router.get("/testAnswer/:testId", GetUserAnswerOfAllTestQuestion);
+
+//update answer
+router.put("/update", UpdateResponse);
+
+//delete answer
+router.delete("/:answerId");
+
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -10,11 +10,13 @@ const AssessmentRoutes = require("./assessment.routes");
 const QuestionRoutes = require("./question.routes");
 //question option routes
 const AnswerOptionRoutes = require("./option.routes");
-
+//user response as answer of a question routes
+const AnswerRoutes = require("./answer.routes"); 
 
 router.use("/user", UserRoutes);
 router.use("/assessment", AssessmentRoutes);
 router.use("/question", QuestionRoutes );
 router.use("/option", AnswerOptionRoutes);
+router.use("/answer", AnswerRoutes);
 
 module.exports = router;
