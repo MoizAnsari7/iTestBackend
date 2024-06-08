b334ca2e9d0e268fc14589484a6e0f9c016dff23
MoizAnsari7 <moizansari176@gmail.com>
2024-06-08 23:21:38 +0530

    populate test list with the assessment in controller

--- models/assessment.model.js
+++ models/assessment.model.js
@@ -1,18 +1,18 @@
-const { Schema, model } = require("mongoose")
+    const { Schema, model } = require("mongoose")
 
-const assessmentSchema = new Schema({
-    name : String,
-    description : String,
-    timeLimit : { type: Number, required: true },//in minutes
-    createdBy : { type: Schema.Types.ObjectId, ref: 'users' },
-    test : [{
-        type : Schema.Types.ObjectId , ref: 'tests'
-    }]
-},{
-    timestamps : true,
-    toJSON : { virtuals: true },
-    toObject : { virtuals: true },
-})
+    const assessmentSchema = new Schema({
+        name : String,
+        description : String,
+        timeLimit : { type: Number, required: true },//in minutes
+        createdBy : { type: Schema.Types.ObjectId, ref: 'users' },
+        test : [{
+            type : Schema.Types.ObjectId , ref: 'tests'
+        }]
+    },{
+        timestamps : true,
+        toJSON : { virtuals: true },
+        toObject : { virtuals: true },
+    })
 
-const assessmentModel = model("assessments", assessmentSchema)
-module.exports = assessmentModel
+    const assessmentModel = model("assessments", assessmentSchema)
+    module.exports = assessmentModel
